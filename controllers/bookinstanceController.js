127946ab7398f7c4b75414482742d35f44bebae3
char <charlen17e@gmail.com>
2019-10-01 21:47:28 -0500

    Finish detail views

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -13,8 +13,23 @@ exports.bookinstance_list = function(req, res, next) {
         res.render('bookinstance_list', {title: 'Book Instance List', bookinstance_list: list_bookinstances});
       });
 };
-exports.bookinstance_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+// exports.bookinstance_detail = function(req, res) {
+//   res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+// };
+
+exports.bookinstance_detail = function(req, res, next) {
+  BookInstance.findById(req.params.id).populate('book')
+      .exec(function(err, results) {
+        if (err) {
+          return next(err);
+        }
+        if (results == null) {
+          const err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+        res.render('bookinstance_detail', {title: 'Copy'+ results.book.title, bookinstance: results});
+      });
 };
 
 exports.bookinstance_create_get = function(req, res) {
--- models/author.js
+++ models/author.js
@@ -1,34 +1,34 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-const moment = require('moment')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const moment = require('moment');
 
-var AuthorSchema = new Schema(
-   {
+const AuthorSchema = new Schema(
+    {
       first_name: {type: String, required: true, max: 100},
-      family_name: {type: String, required:true, max:100},
+      family_name: {type: String, required: true, max: 100},
       date_of_birth: {type: Date},
       date_of_death: {type: Date},
-   }
-)
+    }
+);
 
-AuthorSchema.virtual('name').get(function(){
-   return this.family_name + ',' + this.first_name;
+AuthorSchema.virtual('name').get(function() {
+  return this.family_name + ',' + this.first_name;
 });
 
 AuthorSchema.virtual('lifespan').get(function() {
-   return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  return (this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '').toString() + '  -  ' + (this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : '').toString();
 });
 
 AuthorSchema.virtual('url').get(function() {
-   return '/catalog/author/' + this._id;
+  return '/catalog/author/' + this._id;
 });
 
 AuthorSchema.virtual('date_of_birth_formatted').get(function() {
-   return this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '';
-})
+  return this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '';
+});
 
 AuthorSchema.virtual('date_of_death_formatted').get(function() {
-   return this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : '';
-})
+  return this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : '';
+});
 
 module.exports = mongoose.model('Author', AuthorSchema);
